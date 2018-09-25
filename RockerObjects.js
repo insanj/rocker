783a2d3f06b46bccd2b2680572abaca0452ea78c
insanj <insanj@gmail.com>
2018-09-25 00:07:54 -0400

    wire up rgb->hue color converter prototype, add lights.js example

---
+++ RockerObjects.js
@@ -0,0 +1,83 @@
+
+class RockerConfig {
+  constructor(authLocation, authAttempts, authTimeout) {
+    this.authLocation = authLocation;
+    this.authAttempts = authAttempts;
+    this.authTimeout = authTimeout;
+  }
+}
+
+class RockerAuthenticator {
+  constructor(networker) {
+    this.networker = networker;
+  }
+  
+  authenticate(callback) {
+    var checkIfAuthenticated = function(authBlock, successCallback, failureCallback, numberOfAttempts) {
+    };
+
+    checkIfAuthenticated = function(authBlock, successCallback, failureCallback, numberOfAttempts) {
+      authBlock(function(result) {
+       rocker_log("Checked if user pressed button, result -- " + JSON.stringify(result, null));
+       if (result && result["responseJSON"] && result["responseJSON"][0] && result["responseJSON"][0]["success"] && result["responseJSON"][0]["success"]["username"]) {
+          successCallback(result["responseJSON"][0]["success"]["username"]); // username in response! continue...
+       } else {
+          if (numberOfAttempts-1>0) {
+            checkIfAuthenticated(authBlock, successCallback, failureCallback, numberOfAttempts-1);
+          } else {
+            failureCallback(); // unable to auth after attempts <= 0
+          }
+       }
+      }); 
+    };
+
+    var networker = this.networker;
+    checkIfAuthenticated(function(aCallback) {
+      networker.authenticateWithBridge(aCallback);
+    }, function(username) {
+      callback(username);
+    }, function() {
+      callback(null);
+    }, networker.config.authAttempts); // check n times
+  }
+}
+
+class RockerNetworker {
+  constructor(config){
+    this.config = config;
+  }
+
+  authenticateWithBridge(callback) {
+    var authLocation = this.config.authLocation + "/api/";
+    var authBody = {"devicetype" : "rocker#debug julian"};
+    setTimeout(function() {
+      $.ajax({
+        url: authLocation,
+        method: "POST",
+        data: JSON.stringify(authBody),
+        complete: function (data) {
+          callback(data);
+        }
+      });
+    }, this.config.authTimeout);
+  }
+  
+  toggleLights(lightsOnOrOffState, bridgeUsername, xyVal) {
+    var lightsOffLocation = this.config.authLocation + "/api/" + bridgeUsername + "/groups/3/action";
+    rocker_log("Location =  " + lightsOffLocation);
+    var lightsOffBody = {"on": lightsOnOrOffState, "xy": xyVal};
+    var lightsOffString = JSON.stringify(lightsOffBody);
+
+    rocker_log("Body = " + lightsOffBody + " String = " + lightsOffString);
+    
+    $.ajax({
+      url: lightsOffLocation,
+      data: lightsOffString,
+      method: "PUT",
+      complete: function (data) {
+        rocker_log("Attempted to toggle lights! " + data);
+      }
+    });
+  }
+}
+
--- rocker.js
+++ rocker.js
@@ -1,98 +1,74 @@
 
+function rocker_log(debugString) {
+  $("#content").prepend(debugString + "<br/>----------<br/>");
+}
+
+/////
+
+var rocker_username = "LJrsZ91tP4Btgl7BYf80Ga50cz6n9NhtdGHKkLOw";
+let config = new RockerConfig("http://192.168.0.101", 5, 5000);
+let networker = new RockerNetworker(config);
+let rocker = new RockerAuthenticator(networker);
+
+$("#rocker").submit(function(event) {
+  event.preventDefault();
+  let rockerInputR = $("#red").val();
+  let rockerInputG = $("#green").val();
+  let rockerInputB = $("#blue").val();
+
+  rocker_log("Attempting to convert " + rockerInputR + rockerInputG + rockerInputB);
+  let rockerColorXY = rgb_to_cie(rockerInputR, rockerInputG, rockerInputB);
+  rocker_log("XY = " + rockerColorXY);
+  let rockerColorXYInts = [parseFloat(rockerColorXY[0]), parseFloat(rockerColorXY[1])];
+  networker.toggleLights(true, rocker_username, rockerColorXYInts);
+});
+
 function rocker_main() {
-  let config = new RockerConfig("http://192.168.0.101", 5, 5000);
-  let rocker = new Rocker(config);
-  rocker.authenticate(function(username) {
-    if (username == null) {
-      console.log("Tried to do that auth thing and failed :(");
-    } else {
-      console.log("Finished auth cycle!! :)");
-      rocker.toggleLights(true, username);
-    }
-  });
-}
-
-class RockerConfig {
-  constructor(authLocation, authAttempts, authTimeout) {
-    this.authLocation = authLocation;
-    this.authAttempts = authAttempts;
-    this.authTimeout = authTimeout;
-  }
-}
-
-class Rocker {
-  constructor(config){
-    this.config = config;
-  }
-  
-  authenticate(callback) {
-    console.log("Beginning to authenticate on " + this.config + "...");
-
-    var checkIfAuthenticated = function(authBlock, successCallback, failureCallback, numberOfAttempts) {
-    };
-
-    checkIfAuthenticated = function(authBlock, successCallback, failureCallback, numberOfAttempts) {
-      authBlock(function(result) {
-       console.log("Checked if user pressed button, result -- " + JSON.stringify(result, null));
-       if (result && result["responseJSON"] && result["responseJSON"][0] && result["responseJSON"][0]["success"] && result["responseJSON"][0]["success"]["username"]) {
-          successCallback(result["responseJSON"][0]["success"]["username"]); // username in response! continue...
-       } else {
-          if (numberOfAttempts-1>0) {
-            checkIfAuthenticated(authBlock, successCallback, failureCallback, numberOfAttempts-1);
-          } else {
-            failureCallback(); // unable to auth after attempts <= 0
-          }
-       }
-      }); 
-    };
-
-    var networker = new RockerNetworker(this.config);
-    checkIfAuthenticated(function(callback) {
-      networker.authenticateWithBridge(callback);
-    }, function(username) {
-      callback(username);
-    }, function() {
-      callback(null);
-    }, this.config.authAttempts); // check n times
-  }
-
-  toggleLights(lightsOnOrOffState, username) {
-    var networker = new RockerNetworker(this.config);
-    networker.toggleLights(lightsOnOrOffState, username);
-  }
-}
-
-class RockerNetworker {
-  constructor(config){
-    this.config = config;
-  }
-
-  authenticateWithBridge(callback) {
-    var authLocation = this.config.authLocation + "/api/";
-    var authBody = {"devicetype" : "rocker#debug julian"};
-    setTimeout(function() {
-      $.ajax({
-        url: authLocation,
-        method: "POST",
-        data: JSON.stringify(authBody),
-        complete: function (data) {
-          callback(data);
-        }
-      });
-    }, this.config.authTimeout);
-  }
-  
-  toggleLights(lightsOnOrOffState, bridgeUsername) {
-    var lightsOffLocation = this.config.authLocation + "/api/" + bridgeUsername + "/groups/0/action/";
-    var lightsOffBody = {"on": lightsOnOrOffState};
-    $.ajax({
-      url: lightsOffLocation,
-      data: JSON.stringify(lightsOffBody),
-      method: "PUT",
-      complete: function (data) {
-        console.log("Attempted to toggle lights! " + data);
+  if (rocker_username == null) {
+    rocker_log("Beginning to authenticate on " + config + "...");
+    rocker.authenticate(function(username) {
+      if (username == null) {
+        rocker_log("Tried to do that auth thing and failed :(");
+      } else {
+        rocker_log("Finished auth cycle!! :)");
+        rocker_username = username;
       }
     });
+  } else {
+    rocker_log("Logged in as " + rocker_username);
   }
 }
 
+// https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+function hexToRgb(hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+function do_forever() {
+  var color = randomColor().replace("#", "");
+  rocker_log(color);
+  var colorRGB = hexToRgb(color);
+  rocker_log(colorRGB);
+
+  let rockerColorXY = rgb_to_cie(colorRGB["r"], colorRGB["g"], colorRGB["b"]);
+  rocker_log("colorRGB XY = " + rockerColorXY);
+  let rockerColorXYInts = [parseFloat(rockerColorXY[0]), parseFloat(rockerColorXY[1])];
+  networker.toggleLights(true, rocker_username, rockerColorXYInts);
+
+  setTimeout(function() {
+    do_forever();
+  }, 2000);
+}
+
+do_forever();
